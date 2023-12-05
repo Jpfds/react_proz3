860978ebce1922355462c0ddaf212b54c4ff97d2
Jpfds <jallinrabbei57@gmail.com>
2023-12-05 10:18:58 -0300

    atividade

---
+++ src/funcoes/Mensagem.js
@@ -0,0 +1,12 @@
+import './Mensagem.css';
+
+
+function retornarMensagem() {
+    return (
+      <div class='divMensagem'>
+        <h1>Olá, bom dia, boa tarde e boa noite!!!</h1>
+      </div>  
+    );
+}
+
+export default retornarMensagem;
